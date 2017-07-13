3318bcb8c13226343b1ee495a2db5bc0118d0c11
eduardoasc <eduardo27raul@hotmail.com>
2017-07-13 14:08:32 -0400

    worked on poke roster

--- pokemon-roster.js
+++ pokemon-roster.js
@@ -1,3 +1,8 @@
 var pokemonRoster =[];
 var name = ["magnezone","empoleon","torterra","giratina","togekiss","blazikem"];
-var type = ["electrc, steel", "water, steel", "grass, ground","ghost, dragon"]
+var type = ["electrc, steel", "water, steel", "grass, ground","ghost, dragon","fairy, flying","fire, fighting"];
+var hp = [70,84,95,150,"85","80"];
+var defense = ["115","88","105","100","95","70"];
+var attack = ["70","86","109","120","50","120"];
+var legend = ["false","false","false","true","false","false"];
+
