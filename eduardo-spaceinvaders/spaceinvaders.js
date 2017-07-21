64986afe638720ed26b3792ddc4018d81ac93551
eduardoasc <eduardo27raul@hotmail.com>
2017-07-21 14:28:59 -0400

    space invaders

--- eduardo-spaceinvaders/spaceinvaders.js
+++ eduardo-spaceinvaders/spaceinvaders.js
@@ -6,99 +6,83 @@ function setup(){
 var i = 0
  x = 100
  y = 100
- left = 0
  down = 0
  var alien1 = {
     x:100,
     y:50,
     shown:0
-
 }
 var alien2 = {
     x:200,
     y:50,
     shown:0 
 }
-
 var alien3 = {
     x:300,
     y:50,
     shown:0 
 }
-
 var alien4 = {
     x:100,
     y:100,
     shown:0 
 }
-
 var alien5 = {
     x:200,
     y:100,
     shown:0 
 }
-
 var alien6 = {
     x:300,
     y:100,
     shown:0 
 }
-
 var alien7 = {
     x:100,
     y:150,
     shown:0 
 }
-
 var alien8 = {
     x:200,
     y:150,
     shown:0 
 }
-
 var alien9 = {
     x:300,
     y:150,
     shown:0 
 }
-
 var alien10 = {
     x:100,
     y:200,
     shown:0
 }
-
 var alien11 = {
     x:200,
     y:200,
     shown:0 
 }
-
 var alien12 = {
     x:300,
     y:200,
     shown:0 
 }
-
 var alien13 = {
     x:100,
     y:250,
     shown:0 
 }
-
 var alien14 = {
     x:200,
     y:250,
     shown:0
 }
-
 var alien15 = {
     x:300,
     y:250,
     shown:0 
 }
 var aliens = [alien1,alien2,alien3,alien4,alien5,alien6,alien7,alien8,alien9,alien10,alien11,alien12,alien13,alien14,alien15];
-// triangle(170,550,200,500,230,550)
 var ship = {
     x1:170,
     y1:550,
@@ -109,7 +93,7 @@ var ship = {
 }
 var projectile = {
     x: ship.x2,
-    y: 470,
+    y: 500,
     showProjectile: false
 }
 function keyPressed(){
@@ -154,19 +138,18 @@ function draw(){
         if(aliens[i].shown === 0){
             rect(aliens[i].x,aliens[i].y,75,40)
             checkHit(aliens[i],projectile)
-        }
-    }
-    // time += 1
-    // if(time === 60){
-    //     for(var b=0;b<15;b++){
-    //         if(left === false){
-    //             aliens[b].x += 10
-    //         }else{
-    //             aliens[b].x -= 10
-    //         }
-    //     }
-    //     time = 0
-    // }
+            time += 1;
+            if(time === 60){
+                for(var b=0;b<15;b++){
+                    if(left === false){
+                        aliens[b].x += 10;
+                    }else{
+                        aliens[b].x -= 10;
+                    }
+                }
+            }
+        }time = 0;
+    }    
     if(projectile.showProjectile === true){
         fill(255,255,0)
         rect(projectile.x,projectile.y,10,20)
